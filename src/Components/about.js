a4001c0d8f0192abd2d33783bc1a4f9b87e9ad7f
MCarmody17 <Matthewcarmody17@gmail.com>
2024-07-10 00:59:44 +0200

    Initial commit

---
+++ src/Components/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../styles/about.css";
+
+
+export default function Intro() {
+    return (
+        <div className="aboutMe">
+            <h1>About me:</h1>
+        </div>
+    );
+}
