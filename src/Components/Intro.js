2e1e37596fece6a13652245e6a619eacd6b144d4
MCarmody17 <Matthewcarmody17@gmail.com>
2024-07-29 16:53:27 +0200

    Added email icon

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../styles/intro.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 export default function Intro() {
     return (
@@ -18,7 +20,7 @@ export default function Intro() {
             className="intro-contact"
           >
             
-            {" Email me!"}
+            <FontAwesomeIcon icon={faEnvelope} /> {" Email me!"}
           </a>
         </div>
     );
